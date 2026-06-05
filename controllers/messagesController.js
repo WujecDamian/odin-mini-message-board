13b7f271fa0d379c5bbc0f1ccbbbd35f88d568aa
Damian Wujec <wujecdamian12@gmail.com>
2026-06-05 23:17:25 +0200

    feat: add messages array to  messagesController

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,3 +1,16 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 const getMessages = (req, res) => {
   res.render("home", { messages });
 };
