f55ca7575783be9509f7dbe207a9ad38e0af08e6
Damian Wujec <wujecdamian12@gmail.com>
2026-06-06 01:37:15 +0200

    fix: add route for retrieving new message form

--- routes/newMessageRoutes.js
+++ routes/newMessageRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const newMessagesController = require("../controllers/newMessageController");
 
+router.get("/", newMessagesController.getNewMessageForm);
 router.post("/", newMessagesController.newMessage);
 
 module.exports = router;
