1ab8f6b52646213770b51cb03c025ad3e2cd21d0
Damian Wujec <wujecdamian12@gmail.com>
2026-06-06 01:37:19 +0200

    fix: add getNewMessageForm function to render message form

--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -1,5 +1,18 @@
-const newMessage = (req, res) => {
-  const message = req.body.message;
+const messages = require("./messagesController");
+
+const getNewMessageForm = (req, res) => {
+  res.render("form");
 };
 
-module.exports = { newMessage };
+const newMessage = (req, res) => {
+  console.table(messages);
+  const message = req.body.message;
+  messages.push({
+    text: req.body.message,
+    user: req.body.username,
+    added: new Date(),
+  });
+  res.redirect("/");
+};
+
+module.exports = { getNewMessageForm, newMessage };
