80d4461a7d1e96b7ab19ca1e700a61ef092947f4
Damian Wujec <wujecdamian12@gmail.com>
2026-06-09 01:26:15 +0200

    refactor: rewrite dotenv  require for better readability

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 //Configure ejs
 app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "ejs");
