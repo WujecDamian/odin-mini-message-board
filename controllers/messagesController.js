c75f2028b8df23b8b8b794a316d6e7c458dd8b29
Damian Wujec <wujecdamian12@gmail.com>
2026-06-09 15:35:19 +0200

    refactor: remove hardcoded messages and update getMessages and getMessageDetails to use database retrieval

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,24 +1,17 @@
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
+const db = require("../db/queries");
 
-const getMessages = (req, res) => {
+const getMessages = async (req, res) => {
+  const messages = await db.getAllMessages();
+  console.log(messages);
   res.render("home", { messages });
 };
-const getMessageDetails = (req, res) => {
+const getMessageDetails = async (req, res) => {
+  const messages = await db.getAllMessages();
+
   const url = req.url.split("/");
   const id = url[2];
   const message = messages[id];
   res.render("messageDetails", { message });
 };
 
-module.exports = { getMessages, getMessageDetails, messages };
+module.exports = { getMessages, getMessageDetails };
